f257be408a607ad01a0fc3e8cb485ef3162ede5c
Benedictus Jullian <benedictusjullian1@gmail.com>
2023-10-21 21:53:42 +0700

    complete the layout of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
+import {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  CustomerReviews,
+  Footer,
+  SpecialOffers,
+  Subscribe,
+} from "./sections";
+
 const App = () => {
   return (
     <main className="relative">
       Nav
-      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
-      <section className="padding">PopularProducts</section>
-      <section className="padding">SuperQuality</section>
-      <section className="padding-x py-10">Services</section>
-      <section className="padding">Special Offers</section>
-      <section className="padding bg-pale-blue">Customer Reviews</section>
-      <section className="padding-x sm:py-16 w-full">Subscribe</section>
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffers />
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-16 w-full">
+        <Subscribe />
+      </section>
       <section className="padding bg-black padding-x padding-t pb-8">
-        Footer
+        <Footer />
       </section>
     </main>
   );
