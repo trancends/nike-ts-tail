01b38504bf55a392d97e41289fea0b86a1a838d2
Benedictus Jullian <benedictusjullian1@gmail.com>
2023-10-21 21:41:22 +0700

    create template components for all sections

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,15 @@ const App = () => {
     <main className="relative">
       Nav
       <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+      <section className="padding">PopularProducts</section>
+      <section className="padding">SuperQuality</section>
+      <section className="padding-x py-10">Services</section>
+      <section className="padding">Special Offers</section>
+      <section className="padding bg-pale-blue">Customer Reviews</section>
+      <section className="padding-x sm:py-16 w-full">Subscribe</section>
+      <section className="padding bg-black padding-x padding-t pb-8">
+        Footer
+      </section>
     </main>
   );
 };
