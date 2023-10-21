26cd8c99b9e715b9f4059dc194d42927172dae25
Benedictus Jullian <benedictusjullian1@gmail.com>
2023-10-21 21:24:54 +0700

    remove some packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,9 +3,9 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
-    "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
