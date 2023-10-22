b9e4e852648d58112c47f56f5f3545e58413daa6
Benedictus Jullian <benedictusjullian1@gmail.com>
2023-10-22 13:30:57 +0700

    style navbar and add prettier plugin tailwind to format styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Nav from "./components/Nav";
 import {
   Hero,
   PopularProducts,
@@ -12,7 +13,7 @@ import {
 const App = () => {
   return (
     <main className="relative">
-      Nav
+      <Nav />
       <section className="xl:padding-1 wide:padding-r padding-b">
         <Hero />
       </section>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,15 @@
+import { headerLogo } from "../assets/images";
+
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <header className="padding-x max-container absolute z-10 flex w-full items-center justify-between py-8">
+      <nav>
+        <a href="/">
+          <img src={headerLogo} width={130} height={30} alt="" />
+        </a>
+      </nav>
+    </header>
+  );
 };
 
 export default Nav;
